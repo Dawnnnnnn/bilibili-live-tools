736ffb08b6e759947d974bac8c807e0b9275fe1c
Dawnspace <1050596704@qq.com>
2018-03-18 17:02:37 +0800

    这次不需要那个难安装的库了，顺便加上获取cookie

--- applogin.py
+++ applogin.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
-#coding:utf-8
+# coding:utf-8
+import base64
 import requests
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_v1_5
-from base64 import b64encode
+import rsa
 import hashlib
 from urllib import parse
 
@@ -13,7 +12,7 @@ app_secret = '560c52ccd288fed045859ed18bffd973'
 
 def GetHash():
     url = 'https://passport.bilibili.com/api/oauth2/getKey'
-    temp_params = 'appkey='+appkey+app_secret
+    temp_params = 'appkey=' + appkey + app_secret
     hash = hashlib.md5()
     hash.update(temp_params.encode('utf-8'))
     sign = hash.hexdigest()
@@ -23,25 +22,31 @@ def GetHash():
     return value
 
 
-
 def Login():
-    username = input("请输入用户名:")
-    password = input("请输入密码:")
+    username = input("输入用户名:")
+    password = input("输入密码:")
     key = GetHash()['key']
     Hash = str(GetHash()['hash'])
-    encryptor = PKCS1_v1_5.new(RSA.importKey(bytes(key, 'utf-8')))
-    password = str(b64encode(encryptor.encrypt(bytes(Hash+password, 'utf-8'))), 'utf-8')
+    pubkey = rsa.PublicKey.load_pkcs1_openssl_pem(key.encode())
+    password = base64.b64encode(rsa.encrypt((Hash + password).encode('utf-8'), pubkey))
     password = parse.quote_plus(password)
-    url = 'https://passport.bilibili.com/api/oauth2/login'
-    temp_params = 'appkey='+appkey+'&password='+password+'&username='+username
+    # url = 'https://passport.bilibili.com/api/oauth2/login'   //旧接口
+    url = "https://passport.bilibili.com/api/v2/oauth2/login"
+    temp_params = 'appkey=' + appkey + '&password=' + password + '&username=' + username
     params = temp_params + app_secret
     hash = hashlib.md5()
     hash.update(params.encode('utf-8'))
     sign = hash.hexdigest()
     headers = {"Content-type": "application/x-www-form-urlencoded"}
-    payload = "appkey="+appkey+"&password="+password+"&username="+username+"&sign="+sign
+    payload = "appkey=" + appkey + "&password=" + password + "&username=" + username + "&sign=" + sign
     response = requests.post(url, data=payload, headers=headers)
     print(response.json())
+    print("access_key为：", response.json()['data']['token_info']['access_token'])
+    cookie = (response.json()['data']['cookie_info']['cookies'])
+    cookie_format = ""
+    for i in range(0, len(cookie)):
+        cookie_format = cookie_format + cookie[i]['name'] + "=" + cookie[i]['value'] + ";"
+    print("Cookie为：", cookie_format)
 
 
 Login()
