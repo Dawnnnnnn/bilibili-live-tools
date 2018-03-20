2cd7758b780c852897fd8c07f0026907e8d7dea1
Dawnspace <1050596704@qq.com>
2018-03-20 23:19:48 +0800

    增加应援团签到功能

--- Tasks.py
+++ Tasks.py
@@ -1,10 +1,13 @@
 from login import Login
+import hashlib
 import datetime
 import requests
 import time
 import asyncio
 
+
 class Tasks(Login):
+
     # 获取每日包裹奖励
     def Daily_bag(self):
         url = 'http://api.live.bilibili.com/gift/v2/live/receive_daily_bag'
@@ -36,6 +39,31 @@ class Tasks(Login):
         response3 = requests.post(url, data=payload3, headers=self.appheaders)
         print("今日每日任务已完成")
 
+    # 应援团签到
+    def link_sign(self):
+        url = "https://api.vc.bilibili.com/link_group/v1/member/my_groups"
+        response = requests.get(url,headers=self.pcheaders)
+        check = len(response.json()['data']['list'])
+        group_id_list = []
+        owner_uid_list = []
+        for i in range(0,check):
+            group_id = response.json()['data']['list'][i]['group_id']
+            owner_uid = response.json()['data']['list'][i]['owner_uid']
+            group_id_list.append(group_id)
+            owner_uid_list.append(owner_uid)
+        for (i1,i2) in zip(group_id_list,owner_uid_list):
+            temp_params = "_device="+self.device+"&_hwid=SX1NL0wuHCsaKRt4BHhIfRguTXxOfj5WN1BkBTdLfhstTn9NfUouFiUV&access_key="+self.access_key+"&appkey="+self.appkey+"&build="+self.build+"&group_id="+str(i1)+"&mobi_app="+self.mobi_app+"&owner_id="+str(i2)+"&platform="+self.platform+"&src=xiaomi&trace_id=20171224024300024&ts="+self.CurrentTime()+"&version=5.20.1.520001"
+            params = temp_params + self.app_secret
+            hash = hashlib.md5()
+            hash.update(params.encode('utf-8'))
+            url = "https://api.vc.bilibili.com/link_setting/v1/link_setting/sign_in?"+temp_params+"&sign="+str(hash.hexdigest())
+            response = requests.get(url,headers=self.appheaders)
+            if (response.json()['data']['status']) == 1:
+                print("应援团 %s 已签到过"  %(i1) )
+            if (response.json()['data']['status']) == 0:
+                print("应援团 %s 签到成功,获得 %s 点亲密度"  %(i1,response.json()['data']['add_num']))
+
+    # 扭蛋币
     def redleaf(self):
         url ="http://live.bilibili.com/redLeaf/kingMoneyGift"
         response = requests.get(url,headers=self.pcheaders)
@@ -48,4 +76,5 @@ class Tasks(Login):
             self.Daily_bag()
             self.Daily_Task()
             self.redleaf()
-            await asyncio.sleep(3600)
+            self.link_sign()
+            await asyncio.sleep(21600)
