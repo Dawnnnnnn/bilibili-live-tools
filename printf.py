eb5b8ea6a2964c341c0dc565d125160f062a28ea
nico <13307130285@fudan.edu.cn>
2018-03-27 05:34:19 +0800

    跨平台弹幕接受支持
    就是ios和其他俩分类

--- printf.py
+++ printf.py
@@ -1,5 +1,8 @@
 import webcolors
-import console
+try:
+    import console
+except ImportError:
+    pass
 import configparser
 
 class printf:
@@ -7,7 +10,9 @@ class printf:
         cf = configparser.ConfigParser()
         cf.read("color.conf")
         self.configparser = cf
-        print("初始化")
+        cf.read("user.conf")
+        self.platform = cf.get('platform', 'platform')
+        
 
 
     # "#969696"
@@ -26,8 +31,8 @@ class printf:
         print(str, end=end)
         console.set_color()
         
-    def print_danmu_msg(self, dic):
-        # print(dic)
+    def print_danmu_msg_ios(self, dic):
+        #print(dic)
         info = dic['info']
         tmp = dic['info'][2][1] + ':' + dic['info'][1]
         if info[7] == 3:
@@ -56,4 +61,33 @@ class printf:
         else:
             print(dic['info'][2][1], end='')
         print(':' + dic['info'][1]) 
+    
+    def print_danmu_msg_other(self, dic):
+        info = dic['info']
+        #print(dic)
+        tmp = dic['info'][2][1] + ':' + dic['info'][1]
+        if info[7] == 3:
+            print('舰', end=' ')
+        else:
+            if info[2][3] == 1:
+                if info[2][4] == 0:
+                    # tmp = '爷 ' + tmp
+                    print('月爷', end=' ')
+                else:
+                    print('年老', end=' ')
+            if info[2][2] == 1:
+                print('房管', end=' ')
+            # 勋章
+            if info[3]:
+                print(info[3][1] + '|' + str(info[3][0]), end=' ')
+            # 等级
+            if not info[5]:
+                print('UL' + str(info[4][0]), end=' ')
+        print(tmp)
+        
+    def print_danmu_msg(self, dic):
+        if (self.platform == 'ios_pythonsta'):
+            self.print_danmu_msg_ios(dic)
+        else:
+            self.print_danmu_msg_other(dic)
 
