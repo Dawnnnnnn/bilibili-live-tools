949a2413ceb15936b8c640b8ac44f4f716de2753
Dawnspace <1050596704@qq.com>
2018-04-20 16:54:51 +0800

    bug

--- Tasks.py
+++ Tasks.py
@@ -91,18 +91,18 @@ class Tasks():
                 gift_id = int(temp[i][0])
                 gift_num = int(temp[i][1])
                 bag_id = int(temp[i][2])
-                if (gift_num * (temp_dic[gift_id] / 100) < left_num) and (gift_id != 4 and gift_id != 3 and gift_id != 9 and gift_id != 10):
-                    calculate = calculate + temp_dic[gift_id] / 100 * gift_num
-                    # tmp = calculate / (temp_dic[gift_id] / 100)
-                    tmp2 = temp_dic[gift_id] / 100 * gift_num
-                    await utils.send_gift_web(roomid,gift_id,gift_num,bag_id)
-                    left_num = left_num-tmp2
-                elif left_num - temp_dic[gift_id] / 100 >= 0 and (gift_id != 4 and gift_id != 3 and gift_id != 9 and gift_id != 10):
-                    tmp = (left_num) / (temp_dic[gift_id] / 100)
-                    tmp1 = (temp_dic[gift_id] / 100) * int(tmp)
-                    calculate = calculate + tmp1
-                    await utils.send_gift_web(roomid, gift_id, tmp, bag_id)
-                    left_num = left_num - tmp1
+                if gift_id != 4 and gift_id != 3 and gift_id != 9 and gift_id != 10:
+                    if (gift_num * (temp_dic[gift_id] / 100) < left_num):
+                        calculate = calculate + temp_dic[gift_id] / 100 * gift_num
+                        tmp2 = temp_dic[gift_id] / 100 * gift_num
+                        await utils.send_gift_web(roomid,gift_id,gift_num,bag_id)
+                        left_num = left_num-tmp2
+                    elif left_num - temp_dic[gift_id] / 100 >= 0:
+                        tmp = (left_num) / (temp_dic[gift_id] / 100)
+                        tmp1 = (temp_dic[gift_id] / 100) * int(tmp)
+                        calculate = calculate + tmp1
+                        await utils.send_gift_web(roomid, gift_id, tmp, bag_id)
+                        left_num = left_num - tmp1
             Printer().printlist_append(['join_lottery', '', 'user', "# 自动送礼共送出亲密度为%s的礼物" % int(calculate)])
             
     async def doublegain_coin2silver(self):
