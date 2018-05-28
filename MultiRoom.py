c5fa80d1c288bedc6deca38c45755d23fd264382
Dawnspace <1050596704@qq.com>
2018-05-28 11:59:31 +0800

    fix bug

--- MultiRoom.py
+++ MultiRoom.py
@@ -3,27 +3,31 @@ import random
 
 class MultiRoom():
     def asmr_area(self):
-        url = "https://api.live.bilibili.com/room/v1/area/getRoomList?platform=web&parent_area_id=1&cate_id=0&area_id=0&sort_type=online&page=1&page_size=100"
+        url = "https://api.live.bilibili.com/room/v1/area/getRoomList?platform=web&parent_area_id=1&cate_id=0&area_id=0&sort_type=online&page=1&page_size=-1"
         response = requests.get(url,timeout=3)
-        asmr_area_room = response.json()['data'][0]['roomid']
+        checklen = len(response.json()['data'])
+        asmr_area_room = response.json()['data'][random.randint(0,checklen)]['roomid']
         return asmr_area_room
 
     def game_area(self):
-        url = "https://api.live.bilibili.com/room/v1/area/getRoomList?platform=web&parent_area_id=2&cate_id=0&area_id=0&sort_type=online&page=1&page_size=100"
+        url = "https://api.live.bilibili.com/room/v1/area/getRoomList?platform=web&parent_area_id=2&cate_id=0&area_id=0&sort_type=online&page=1&page_size=-1"
         response = requests.get(url,timeout=3)
-        game_area_room = response.json()['data'][random.randint(0,30)]['roomid']
+        checklen = len(response.json()['data'])
+        game_area_room = response.json()['data'][random.randint(0,checklen)]['roomid']
         return game_area_room
 
     def mobile_area(self):
-        url = "https://api.live.bilibili.com/room/v1/area/getRoomList?platform=web&parent_area_id=3&cate_id=0&area_id=0&sort_type=online&page=1&page_size=100"
+        url = "https://api.live.bilibili.com/room/v1/area/getRoomList?platform=web&parent_area_id=3&cate_id=0&area_id=0&sort_type=online&page=1&page_size=-1"
         response = requests.get(url,timeout=3)
-        mobile_area_room = response.json()['data'][random.randint(0,30)]['roomid']
+        checklen = len(response.json()['data'])
+        mobile_area_room = response.json()['data'][random.randint(0,checklen)]['roomid']
         return mobile_area_room
 
     def draw_area(self):
-        url = "https://api.live.bilibili.com/room/v1/area/getRoomList?platform=web&parent_area_id=4&cate_id=0&area_id=0&sort_type=online&page=1&page_size=100"
+        url = "https://api.live.bilibili.com/room/v1/area/getRoomList?platform=web&parent_area_id=4&cate_id=0&area_id=0&sort_type=online&page=1&page_size=-1"
         response = requests.get(url,timeout=3)
-        draw_area_room = response.json()['data'][random.randint(0,30)]['roomid']
+        checklen = len(response.json()['data'])
+        draw_area_room = response.json()['data'][random.randint(0,checklen)]['roomid']
         return draw_area_room
 
     def get_all(self):
@@ -34,7 +38,8 @@ class MultiRoom():
             draw = self.draw_area()
             return [asmr,game,mobile,draw]
         except:
-            self.get_all()
+            tmp = self.get_all()
+            return tmp
 
 
 
